c5da309018ae0b43149dde452c61a424cb415d20
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-28 01:49:38 +0200

    bebok update

--- Bebok/Assets/BebokController.cs
+++ Bebok/Assets/BebokController.cs
@@ -2,37 +2,19 @@ using UnityEngine;
 
 public class BebokController : MonoBehaviour
 {
-    Rigidbody2D rb;
     [SerializeField]
     float moveSpeed = 10f;
 
-    private Vector2 moveInput;
+    private float moveX, moveY;
 
-    private void Start()
+
+    void FixedUpdate()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        Vector3 moveVector = new Vector3(moveX, moveY);
 
-    private void Update()
-    {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-
-        moveInput.Normalize();
-
-        rb.velocity = moveInput * moveSpeed;
-
-        RaycastHit hit;
-
-        Vector3 p1 = transform.position + this.transform.position;
-        float distanceToObstacle = 0;
-
-        // Cast a sphere wrapping character controller 10 meters forward
-        // to see if it is about to hit anything.
-        if (Physics.SphereCast(p1, 0, transform.forward, out hit, 10))
-        {
-            Debug.Log(distanceToObstacle = hit.distance);
-        }
+        transform.Translate(moveVector * Time.deltaTime);
     }
 
     public void SetSpeed(float newSpeed)
