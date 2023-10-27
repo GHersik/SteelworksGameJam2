ff7255c023db7aa1bdc12967828a410cf9c51e67
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-28 01:04:17 +0200

    bebok pref

--- Bebok/Assets/BebokController.cs
+++ Bebok/Assets/BebokController.cs
@@ -21,6 +21,18 @@ public class BebokController : MonoBehaviour
         moveInput.Normalize();
 
         rb.velocity = moveInput * moveSpeed;
+
+        RaycastHit hit;
+
+        Vector3 p1 = transform.position + this.transform.position;
+        float distanceToObstacle = 0;
+
+        // Cast a sphere wrapping character controller 10 meters forward
+        // to see if it is about to hit anything.
+        if (Physics.SphereCast(p1, 0, transform.forward, out hit, 10))
+        {
+            Debug.Log(distanceToObstacle = hit.distance);
+        }
     }
 
     public void SetSpeed(float newSpeed)
