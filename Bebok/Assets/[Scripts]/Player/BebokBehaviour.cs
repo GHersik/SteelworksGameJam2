ea91f158a98e71bdefe5f23aec08d85b54a1d0bd
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-28 18:46:50 +0200

    no message

--- Bebok/Assets/[Scripts]/Player/BebokBehaviour.cs
+++ Bebok/Assets/[Scripts]/Player/BebokBehaviour.cs
@@ -7,10 +7,14 @@ public class BebokBehaviour : MonoBehaviour {
     [SerializeField, Range(0, 1)] float fadeAmount = 0.3f;
     [SerializeField] float fadeTime = 1f;
     [SerializeField] SpriteRenderer sprite;
+    [SerializeField] GameObject enemyChaser;
+    [SerializeField] GameObject becik;
 
     Color spriteAlpha = Color.white;
     float currentFadeAmount;
     float amountToFadeTo;
+
+    bool becikCanBePickedUp = false;
 
     private void Start() {
         currentFadeAmount = 1;
@@ -20,6 +24,11 @@ public class BebokBehaviour : MonoBehaviour {
     private void Update() {
         currentFadeAmount = Mathf.Lerp(currentFadeAmount, amountToFadeTo, fadeTime * Time.deltaTime);
         AlterAlpha();
+        if (Input.GetKey(KeyCode.E) && becikCanBePickedUp)
+        {
+            var becikController = becik.GetComponent<BecikController>();
+            becikController.TransportChild(this.transform);
+        }
     }
 
     private void AlterAlpha() {
@@ -30,12 +39,30 @@ public class BebokBehaviour : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("BlindSpot")) {
             amountToFadeTo = fadeAmount;
+            enemyChaser.SetActive(false);
+        }
+        if (collision.transform.gameObject.layer == 6)//6layer to enenmy
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.transform.gameObject.name == "Becik")
+        {
+            becikCanBePickedUp = true;
+            becik = collision.transform.gameObject;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("BlindSpot")) {
             amountToFadeTo = 1;
+            enemyChaser.SetActive(true);
+        }
+        if (collision.transform.gameObject.name == "Becik")
+        {
+
+            becikCanBePickedUp = false;
+            becik = null;
         }
     }
 }
