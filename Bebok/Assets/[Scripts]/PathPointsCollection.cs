9fb68a5624a35d0f4dfd41cb5db1aa1362678c6d
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-28 20:44:07 +0200

    prefaby i awake

--- Bebok/Assets/[Scripts]/PathPointsCollection.cs
+++ Bebok/Assets/[Scripts]/PathPointsCollection.cs
@@ -7,6 +7,13 @@ public class PathPointsCollection : MonoBehaviour
    [SerializeField]
    List<Transform> points = new List<Transform>();
 
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points.Add(gameObject.transform.GetChild(i));
+        }
+    }
     public List<Transform> ListPoints()
     { 
         return points; 
