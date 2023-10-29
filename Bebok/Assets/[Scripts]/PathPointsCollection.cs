e7a4ef2ce010b8a2f5c6a600a0acfe166e3060a0
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-29 13:45:59 +0100

    baby_carry

--- Bebok/Assets/[Scripts]/PathPointsCollection.cs
+++ Bebok/Assets/[Scripts]/PathPointsCollection.cs
@@ -1,14 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PathPointsCollection : MonoBehaviour
 {
-   [SerializeField]
-   List<Transform> points = new List<Transform>();
+    [SerializeField]
+    List<Transform> points = new List<Transform>();
 
     public List<Transform> ListPoints()
-    { 
-        return points; 
+    {
+        return points;
     }
 }
