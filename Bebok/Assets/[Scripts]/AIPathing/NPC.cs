1070d74451650e4167d16ff919dc6a36a1708772
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-29 15:06:47 +0100

    time fix

--- Bebok/Assets/[Scripts]/AIPathing/NPC.cs
+++ Bebok/Assets/[Scripts]/AIPathing/NPC.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NPC : MonoBehaviour {
+public class NPC : MonoBehaviour
+{
     [SerializeField] Transform pathPoint;
     [SerializeField] List<Transform> targetPoints;
     [SerializeField] PathPointsCollection pathPointsCollection;
@@ -12,39 +13,51 @@ public class NPC : MonoBehaviour {
     NavMeshAgent agent;
     PathPointController pathPointController;
 
-    private void Start() {
+    private void Start()
+    {
         pathPointsCollection = FindAnyObjectByType<PathPointsCollection>();
         targetPoints = pathPointsCollection.ListPoints();
         agent = GetComponent<NavMeshAgent>();
         pathPoint = GetRandomTargetPoint();
         StartCoroutine(WaitNearPoint(pathPointController == null ? 1f : pathPointController.WaitingFarThisPoint()));
     }
-    private void Update() {
-        if (!agent.hasPath) {
+    private void Update()
+    {
+        if (pathPoint == null)
+        {
             pathPoint = GetRandomTargetPoint();
             StartCoroutine(WaitNearPoint(pathPointController == null ? 1f : pathPointController.WaitingFarThisPoint()));
+        }
+        else if (!agent.hasPath)
+        {
+            pathPoint = null;
         }
 
         animator.SetBool("isWalking", agent.hasPath);
     }
 
-    public void SetPathToBebok(Transform bebok) {
+    public void SetPathToBebok(Transform bebok)
+    {
         pathPoint = bebok;
         StartCoroutine(WaitNearPoint(pathPointController == null ? 1f : pathPointController.WaitingFarThisPoint()));
     }
 
-    void GoToDestination() {
-        agent.SetDestination(new Vector3(pathPoint.position.x, pathPoint.position.y, 0));
+    void GoToDestination()
+    {
+        if (pathPoint != null)
+            agent.SetDestination(new Vector3(pathPoint.position.x, pathPoint.position.y, 0));
     }
 
-    Transform GetRandomTargetPoint() {
+    Transform GetRandomTargetPoint()
+    {
         int randomPoint = Random.Range(0, targetPoints.Count);
         pathPointController = targetPoints[randomPoint].GetComponent<PathPointController>();
         return targetPoints[randomPoint];
     }
 
-    IEnumerator WaitNearPoint(float waiting) {
-        yield return new WaitForSecondsRealtime(waiting);
+    IEnumerator WaitNearPoint(float waiting)
+    {
+        yield return new WaitForSeconds(waiting * Time.deltaTime);
         GoToDestination();
     }
 
