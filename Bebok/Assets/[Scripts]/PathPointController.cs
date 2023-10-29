4d2b21195be6e0934c855e94399d68b856280f0d
GHersik <80617931+GHersik@users.noreply.github.com>
2023-10-29 09:56:36 +0100

    Level01

--- Bebok/Assets/[Scripts]/PathPointController.cs
+++ Bebok/Assets/[Scripts]/PathPointController.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class PathPointController : MonoBehaviour
-{
-    [SerializeField] float waitingNearThisPoint = 1f;
-    public float WaitingFarThisPoint()
-    {
-        return waitingNearThisPoint;
+public class PathPointController : MonoBehaviour {
+    [SerializeField, Range(0, 3)] float minWaitTime = 1f;
+    [SerializeField, Range(0, 3)] float maxWaitTime = 1f;
+
+    public float WaitingFarThisPoint() {
+        return Random.Range(minWaitTime, maxWaitTime);
     }
 }
--- Bebok/Assets/[Scripts]/RayCastController.cs
+++ Bebok/Assets/[Scripts]/RayCastController.cs
@@ -1,22 +1,17 @@
 using UnityEngine;
 
-public class RayCastController : MonoBehaviour
-{
-    float distance = 15f;
-    [SerializeField]
-    NPC npc;
+public class RayCastController : MonoBehaviour {
+    [SerializeField] float distance = 15f;
+    [SerializeField] NPC npc;
 
-    private void Start()
-    {
+    private void Start() {
         Physics2D.queriesStartInColliders = false;
         npc = this.transform.parent.GetComponent<NPC>();
     }
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, distance);
-        if (hit.collider != null)
-        {
+        if (hit.collider != null) {
             if (hit.collider.gameObject.name == "EnemyChaser")//3 to Bebok layer
                 npc.SetPathToBebok(hit.transform);
         }
