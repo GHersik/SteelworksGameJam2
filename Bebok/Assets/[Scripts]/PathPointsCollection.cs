aa09cf35fc3cec5fd111578e58f5d890a222499c
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-29 13:32:18 +0100

    no message

--- Bebok/Assets/[Scripts]/PathPointsCollection.cs
+++ Bebok/Assets/[Scripts]/PathPointsCollection.cs
@@ -7,13 +7,6 @@ public class PathPointsCollection : MonoBehaviour
    [SerializeField]
    List<Transform> points = new List<Transform>();
 
-    private void Awake()
-    {
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            points.Add(gameObject.transform.GetChild(i));
-        }
-    }
     public List<Transform> ListPoints()
     { 
         return points; 
