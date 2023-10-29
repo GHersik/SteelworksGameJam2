b70c431bc58aa0ccaa39b4063a885dc5af3ca7b5
Bo Ignasiak <boignasiak@protonmail.com>
2023-10-29 17:24:18 +0100

    MusicSystem

--- Bebok/Assets/[Scripts]/Sound/MusicManager.cs
+++ Bebok/Assets/[Scripts]/Sound/MusicManager.cs
@@ -5,10 +5,14 @@ public class MusicManager : SingletonPersistent<MusicManager> {
     [Header("Settings")]
     [SerializeField] AudioSource firstChannel;
     [SerializeField] AudioSource secondChannel;
+    [SerializeField] AudioSource thirdChannel;
     [SerializeField] float soundFadeTime = 1f;
+   [SerializeField] float soundHardcoreFadeTime = 1f;
+   [SerializeField] Ease fadeCurve;
 
     [Header("Audio Clips")]
     [SerializeField] AudioClip backgroundMusic;
+    [SerializeField] AudioClip backgroundMusicHard;
     [SerializeField] AudioClip menuMusic;
 
     private void Start() {
@@ -16,12 +20,21 @@ public class MusicManager : SingletonPersistent<MusicManager> {
     }
 
     public void MainMenuTransition() {
-        firstChannel.DOFade(0, soundFadeTime);
-        secondChannel.DOFade(1,soundFadeTime);
+        firstChannel.DOFade(1, soundFadeTime);
+        secondChannel.DOFade(0,soundFadeTime);
+        thirdChannel.DOFade(0,soundFadeTime);
     }
 
     public void NormalSceneTransition() {
-        firstChannel.DOFade(1, soundFadeTime);
-        secondChannel.DOFade(0, soundFadeTime);
+        firstChannel.DOFade(0, soundFadeTime);
+        secondChannel.DOFade(1, soundFadeTime);
+        thirdChannel.DOFade(0, soundHardcoreFadeTime);
+    }
+
+        public void HardcoreTransition() {
+        thirdChannel.DOFade(1, soundHardcoreFadeTime).SetEase(fadeCurve);
+    }
+        public void SoftcoreTransition() {
+        thirdChannel.DOFade(0, soundHardcoreFadeTime).SetEase(fadeCurve);
     }
 }
