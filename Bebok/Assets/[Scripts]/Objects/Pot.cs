b13b4f783b54018532134e0b9595aef6481c8b57
GHersik <80617931+GHersik@users.noreply.github.com>
2023-10-29 16:21:00 +0100

    Adds Bush sounds.

--- Bebok/Assets/[Scripts]/Objects/Pot.cs
+++ Bebok/Assets/[Scripts]/Objects/Pot.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Pot : MonoBehaviour {
     [SerializeField] ParticleSystem particleSystem;
-    //[SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource audioSource;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             particleSystem.Play();
+            audioSource.Play();
         }  
     }
 
