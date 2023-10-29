4e650a9ad7c11e962b29e2ddfa0fa820f81d04db
Bo Ignasiak <boignasiak@protonmail.com>
2023-10-29 17:29:01 +0100

    Revert "MusicSystem"
    This reverts commit b70c431bc58aa0ccaa39b4063a885dc5af3ca7b5.

--- Bebok/Assets/[Scripts]/BecikController.cs
+++ Bebok/Assets/[Scripts]/BecikController.cs
@@ -54,7 +54,6 @@ public class BecikController : MonoBehaviour
         becikIsNotInHisPlace = true;
         carrier = _carrier;
         crySource.Play();
-        MusicManager.Instance.HardcoreTransition();
     }
 
     public void BecikPickedByFamily(Transform _carrier)
@@ -75,7 +74,6 @@ public class BecikController : MonoBehaviour
     {
         bebokController.speedModifier = 1f;
         carrier = null;
-        MusicManager.Instance.SoftcoreTransition();
     }
     IEnumerator AlertOthers()
     {
