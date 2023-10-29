20da92ffc8d959a6fea533db658140d132f17803
Pawel <pawelek.tokarczyk@gmail.com>
2023-10-29 13:45:59 +0100

    baby_carry

---
+++ Bebok/Assets/CraddleController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CraddleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Becik")
+        {
+            collision.gameObject.GetComponent<BecikController>().PlaceBecikInCraddle(this.transform);
+        }
+    }
+}
